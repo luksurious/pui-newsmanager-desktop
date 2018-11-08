2fd7f026ef1160b487930ba870d80a30062e2655
Lukas Brückner <me@luksurious.me>
2018-11-08 18:51:18 +0100

    update main view: fix image location, generate basic html for  main view, load categories

--- src/application/NewsReaderController.java
+++ src/application/NewsReaderController.java
@@ -3,10 +3,18 @@
  */
 package application;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
 import java.util.function.Predicate;
+
+import javax.imageio.ImageIO;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
@@ -22,8 +30,10 @@ import application.utils.exceptions.ErrorMalFormedNews;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -61,26 +71,34 @@ public class NewsReaderController {
 
 	@FXML
 	ListView<Article> newsList;
-	@FXML Label headline;
-	@FXML Button btnAdd;
-	@FXML Button btnLogin;
-	@FXML ListView<Categories> categoriesList;
-	@FXML WebView newsWebArea;
-	@FXML ImageView headImage;
+	@FXML
+	Label headline;
+	@FXML
+	Button btnAdd;
+	@FXML
+	Button btnLogin;
+	@FXML
+	ListView<Categories> categoriesList;
+	@FXML
+	WebView newsWebArea;
+	@FXML
+	ImageView headImage;
 
 	// TODO add attributes and methods as needed
-	
+	ObservableList<Categories> categoryList;
 
 	@FXML
 	void initialize() {
-		
+
 		// ObservableList category new ObservableList<Categories>()
-		// this.categoriesList.setItems();
-		
+		this.categoriesList.setItems(this.categoryList);
+
 		WebEngine webEngine = this.newsWebArea.getEngine();
-		webEngine.loadContent("<h1>Hello</h1>");
-		
-//		headImage.setImage(new Image(this.getClass().getResource("eit-logo.png").toString(), true));
+		webEngine.loadContent("<h1>Loading...</h1>");
+
+		SimpleDateFormat headFormat = new SimpleDateFormat("EEE, dd. MMMMM YYYY");
+
+		this.headline.setText("These are the news for today, " + headFormat.format(new Date()));
 	}
 
 	public NewsReaderController() {
@@ -88,13 +106,38 @@ public class NewsReaderController {
 		// newsReaderModel.setDummyDate(false);
 		// Get text Label
 
+		this.categoryList = FXCollections.observableArrayList();
+		this.categoryList.addAll(Categories.values());
 	}
 
 	private void getData() {
 		// TODO retrieve data and update UI
 		// The method newsReaderModel.retrieveData() can be used to retrieve data
 		this.newsReaderModel.retrieveData();
-		// this.newsList.setItems(this.newsReaderModel.getArticles());
+
+		String html = "";
+		for (Article article : this.newsReaderModel.getArticles()) {
+			BufferedImage bImage = SwingFXUtils.fromFXImage(article.getImageData(), null);
+			ByteArrayOutputStream s = new ByteArrayOutputStream();
+			String base64Image = "";
+			try {
+				ImageIO.write(bImage, "png", s);
+				byte[] res = s.toByteArray();
+				s.close(); // especially if you are using a different output stream.
+				base64Image = Base64.getEncoder().encodeToString(res);
+				base64Image = "data:image/png;base64," + base64Image;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			html += String.format("<div><img style=\"width:400px;\" src=\"%s\"><br><h2>%s</h2><h4>%s</h4><p>%s</p></div>", base64Image,
+					article.getTitle(), article.getSubtitle(), article.getAbstractText());
+		}
+		WebEngine webEngine = this.newsWebArea.getEngine();
+		
+		html = String.format("<div style=\"font-family:'Segoe UI', sans-serif;padding:20px;\">%s</div>", html);
+		webEngine.loadContent(html);
 	}
 
 	/**
